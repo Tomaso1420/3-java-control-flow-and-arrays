574bce438b36380163efd16b65a06828ad1ad2c8
Tomaso1420 <Artem82005@mail.ru>
2024-09-13 13:37:21 +0500

    Марциохо Артем задание 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,37 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        swap(swap(arr, 0), 1);
+
     }
 
+    static int[] swap(int[] arr, int num) { // num - index of element which needs be swapped with min
+        int min = arr[num];
+        int numMin = 0;
+        for (int i = num + 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (min == arr[i]) {
+                numMin = i;
+            }
+        }
+        int temp = arr[num];
+        arr[num] = arr[numMin];
+        arr[numMin] = temp;
+        return arr;
+    }
 }
